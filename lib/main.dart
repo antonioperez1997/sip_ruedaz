61c4a73ce61574b48343be48557386d8398ced34
JuanDF <juandavidfontalvosilvera@gmail.com>
2020-11-10 16:45:30 -0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Container(
-            child: Text('Hello'),
+            child: Text('Hello flutter'),
           ),
         ),
       ),
